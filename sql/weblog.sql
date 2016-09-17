b190bdd17c38c714eb82d2d7879ff50decc16f87
Colm Hickey <colmohici@gmail.com>
2016-09-17 17:31:59 +0100

    add schema for missing database row

---
+++ sql/weblog.sql
@@ -0,0 +1,39 @@
+--
+-- Table structure for table `web_log`
+--
+
+CREATE TABLE IF NOT EXISTS `web_log` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `message` varchar(255) NOT NULL,
+  `when` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Triggers `web_log`
+--
+DELIMITER $$
+CREATE TRIGGER `datetimefix` BEFORE INSERT ON `web_log`
+ FOR EACH ROW SET NEW.when = NOW()
+$$
+DELIMITER ;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `web_log`
+--
+ALTER TABLE `web_log`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `web_log`
+--
+ALTER TABLE `web_log`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
