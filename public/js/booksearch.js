462147ec6905ee52f706525c7a358587a81635cb
Sebastian Nygren <headswe92@gmail.com>
2017-09-29 10:00:57 +0200

    Add books! (#28)

---
+++ public/js/booksearch.js
@@ -0,0 +1,54 @@
+function search() {
+    $(".segment").addClass("loading");
+    $(".table tbody").empty();
+    $.ajax({
+        url: "/find-books",
+        data: {
+            title: $("#searchInput").val()
+        },
+        type: "GET"
+    }).done(function (data) {
+        if (data.success === true) {
+            $.each(data.rows, function (index, value) {
+                var str = '<tr><td><h4 class="ui image header"><img src="https://robohash.org/'+value.title+'?size50x50" class="ui mini rounded image">'
+                str += '<div class="content"><a href="/book/'+value.id+'">'+value.title+'</a>'
+                str += '<div class="sub header">'+value.author+'</div></div></td></tr>'
+                $(".table tbody").append(str);
+            });
+        } else {
+            alert(data.message);
+        }
+    }).always(function (data) {
+            $(".segment").removeClass("loading");
+    });
+}
+
+
+$(function () { //shorthand document.ready function
+    $.ajax({
+        url: "/get-books",
+        data: {},
+        type: "GET"
+    }).done(function (data) {
+        if (data.success === true) {
+            $.each(data.rows, function (index, value) {
+                var str = '<tr><td><h4 class="ui image header"><img src="https://robohash.org/'+value.title+'?size50x50" class="ui mini rounded image">'
+                str += '<div class="content"><a href="/book/'+value.id+'">'+value.title+'</a>'
+                str += '<div class="sub header">'+value.author+'</div></div></td></tr>'
+                $(".table tbody").append(str);
+            });
+        } else {
+            alert(data.message);
+        }
+    })
+    $("#searchButton").click(function (e) {
+            e.preventDefault();
+            search();
+    });
+    $('body').on('keypress', 'input', function(args) {
+    if (args.keyCode == 13) {
+        $("#searchButton").click();
+        return false;
+    }
+});
+});
---
+++ routes/books.js
@@ -0,0 +1,160 @@
+module.exports = function (app, pool,Logger) {
+    app.get('/get-books', function (req, res) {
+        if (!req.user) {
+            res.sendStatus(500);
+            return;
+        }
+        var options = {
+            skip: 0,
+            limit: 10
+        };
+        if (req.query.skip)
+            options.skip = Number(req.query.skip);
+        if (req.query.limit)
+            options.limit = Number(req.query.limit);
+        pool.getConnection(function (err, connection) {
+            if (err) {
+                console.log(err);
+                res.send(500);
+            }
+            connection.query('SELECT * FROM `library` ORDER BY `title` DESC LIMIT ?,?', [options.skip, options.limit], function (err, rows) {
+                if (err) {
+                    var data = {
+                        success: false,
+                        message: err.message
+                    };
+                    res.send(data);
+                } else {
+                    var data = {
+                        success: true,
+                        rows: rows
+                    };
+                    res.send(data);
+                }
+                connection.release();
+            });
+        });
+    });
+    app.get('/find-books', function (req, res) {
+        if (!req.user) {
+            res.sendStatus(500);
+            return;
+        }
+        if(!req.query.title) {
+            res.sendStatus(500);
+            return;
+        }
+        pool.getConnection(function (err, connection) {
+            if (err) {
+                console.log(err);
+                res.send(500);
+            }
+            connection.query('SELECT id,title,author FROM `library` WHERE title LIKE ? LIMIT 10', ["%"+req.query.title+"%"], function (err, rows) {
+                if (err) {
+                    var data = {
+                        success: false,
+                        message: err.message
+                    };
+                    res.send(data);
+                } else {
+                    var data = {
+                        success: true,
+                        rows: rows
+                    };
+                    res.send(data);
+                }
+                connection.release();
+            });
+        });
+    });
+    app.get('/total-books', function (req, res) {
+        if (!req.user) {
+            res.sendStatus(500);
+            return;
+        }
+        pool.getConnection(function (err, connection) {
+            if (err) {
+                console.log(err);
+                res.send(500);
+            }
+            connection.query('SELECT COUNT(id) FROM `library`', function (err, rows) {
+                if (err) {
+                    var data = {
+                        success: false,
+                        message: err.message
+                    };
+                    res.send(data);
+                } else {
+                    var data = {
+                        success: true,
+                        rows: rows
+                    };
+                    res.send(data);
+                }
+                connection.release();
+            });
+        });
+    });
+    app.get('/get-book', function (req, res) {
+        if (!req.user || !req.query.id) {
+            res.sendStatus(500);
+            return;
+        }
+        var options = {
+            id: Number(req.query.id)
+        };
+        pool.getConnection(function (err, connection) {
+            if (err) {
+                console.log(err);
+                res.send(500);
+            }
+            connection.query('SELECT * FROM `library` WHERE id=?',[options.id], function (err, rows) {
+                if (err) {
+                    var data = {
+                        success: false,
+                        message: err.message
+                    };
+                    res.send(data);
+                } else {
+                    var data = {
+                        success: true,
+                        rows: rows
+                    };
+                    res.send(data);
+                }
+                connection.release();
+            });
+        });
+    });
+    app.post('/delete-book', function (req, res) {
+        if (!req.user || !req.body.id) {
+            res.sendStatus(500);
+            return;
+        }
+        var options = {
+            id: Number(req.body.id)
+        };
+        pool.getConnection(function (err, connection) {
+            if (err) {
+                console.log(err);
+                res.send(500);
+            }
+            Logger.log(req.user, req.user.username + " has deleted a book ("+req.body.id+")");
+            connection.query('DELETE FROM `library` WHERE id=?',[options.id], function (err, rows) {
+                if (err) {
+                    var data = {
+                        success: false,
+                        message: err.message
+                    };
+                    res.send(data);
+                } else {
+                    var data = {
+                        success: true
+                    };
+                    res.send(data);
+                }
+                connection.release();
+            });
+        });
+    });
+};
