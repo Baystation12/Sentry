857f2d060fd14554e575e3224cd3a09e1ae739d4
Sebastian Nygren <headswe92@gmail.com>
2017-10-26 16:31:03 +0200

    Optimized avatar loading, removed random avatars for books. (#31)

--- public/js/booksearch.js
+++ public/js/booksearch.js
@@ -32,7 +32,7 @@ $(function () { //shorthand document.ready function
     }).done(function (data) {
         if (data.success === true) {
             $.each(data.rows, function (index, value) {
-                var str = '<tr><td><h4 class="ui image header"><img src="https://robohash.org/'+value.title+'?size50x50" class="ui mini rounded image">'
+                var str = '<tr><td><h4 class="ui header"><i class="book icon"></i></h4>'
                 str += '<div class="content"><a href="/book/'+value.id+'">'+value.title+'</a>'
                 str += '<div class="sub header">'+value.author+'</div></div></td></tr>'
                 $(".table tbody").append(str);
